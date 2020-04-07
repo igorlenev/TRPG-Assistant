f4702206db8d2541d6fa732bcdcff8dd4f068536
Igor <tankys03@gmail.com>
2020-04-07 18:13:58 +0300

    generateDice() works

--- TRPG-Assistant/app/src/main/java/com/example/trpgassistant/MainActivity.java
+++ TRPG-Assistant/app/src/main/java/com/example/trpgassistant/MainActivity.java
@@ -2,15 +2,32 @@ package com.example.trpgassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.trpgassistant.dice.Dice;
 
 public class MainActivity extends AppCompatActivity {
+    private Button drawButton;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Dice dice = new Dice();
+        editText = findViewById(R.id.numberOfEdges);
+        drawButton = findViewById(R.id.drawButton);
+        drawButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(MainActivity.this, new Dice(Integer.valueOf(editText.getText().toString())).generateDice(), Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
     }
 }
---
+++ TRPG-Assistant/app/src/main/java/com/example/trpgassistant/dice/Dice.java
@@ -0,0 +1,21 @@
+package com.example.trpgassistant.dice;
+
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.trpgassistant.R;
+
+public class Dice extends AppCompatActivity {
+    int dice;
+
+    public Dice (int dice){
+        this.dice = dice;
+        return;
+    }
+
+    public String generateDice (){
+        return String.valueOf((int)(Math.random()*dice+1));
+    }
+}
